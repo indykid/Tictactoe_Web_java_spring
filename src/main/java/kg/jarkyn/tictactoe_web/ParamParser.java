706ea528e696770cce9467cbae0f7fad7ce4c648
jarkyn <jarkyn@ymail.com>
2015-12-10 15:38:08 +0000

    parses string to Mark

--- src/main/java/kg/jarkyn/tictactoe_web/ParamParser.java
+++ src/main/java/kg/jarkyn/tictactoe_web/ParamParser.java
@@ -1,6 +1,7 @@
 package kg.jarkyn.tictactoe_web;
 
 import kg.jarkyn.GameOption;
+import kg.jarkyn.Mark;
 
 public class ParamParser {
     public static int parseNumeric(String numeric) throws NumberFormatException {
@@ -9,5 +10,16 @@ public class ParamParser {
 
     public static GameOption parseGameOption(String numericGameOption) {
         return GameOption.parse(parseNumeric(numericGameOption));
+    }
+
+    public static Mark[] parseMoves(String moves) {
+        String[] splitMoves = moves.split("-");
+        Mark[] marks = new Mark[splitMoves.length];
+
+        for (int i = 0; i < splitMoves.length; i++) {
+            marks[i] = Mark.toMark(splitMoves[i]);
+        }
+
+        return marks;
     }
 }
--- src/test/java/kg/jarkyn/tictactoe_web/ParamParserTest.java
+++ src/test/java/kg/jarkyn/tictactoe_web/ParamParserTest.java
@@ -1,8 +1,12 @@
 package kg.jarkyn.tictactoe_web;
 
 import kg.jarkyn.GameOption;
+import kg.jarkyn.Mark;
 import org.junit.Test;
 
+import static kg.jarkyn.Mark.NONE;
+import static kg.jarkyn.Mark.X;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class ParamParserTest {
@@ -20,5 +24,11 @@ public class ParamParserTest {
     public void parsesIntoGameOption() {
         String numericGameOptionAiFirst = "1";
         assertEquals(GameOption.AI_FIRST, ParamParser.parseGameOption(numericGameOptionAiFirst));
+    }
+
+    @Test
+    public void parsesMoves() {
+        Mark[] expectedMoves = new Mark[]{X, NONE, NONE, NONE, NONE, NONE, NONE, NONE, NONE};
+        assertArrayEquals(expectedMoves, ParamParser.parseMoves("X-NONE-NONE-NONE-NONE-NONE-NONE-NONE-NONE"));
     }
 }
